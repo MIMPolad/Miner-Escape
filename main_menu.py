386fd8da4f20e76df7ab7744675ce718f1fd7180
MIMPolad <116120965+MIMPolad@users.noreply.github.com>
2022-11-05 11:18:34 -0500

    starting to add comments

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,7 @@
 import pygame, sys
 import webbrowser
 from button import Button
+from game import *
 
 pygame.init()
 
@@ -15,29 +16,42 @@ def get_font(size): # Returns font 1 in the desired size
 def get_font2(size): # returns font 2 in desired size
     return pygame.font.Font("Assets/creditsFont.ttf", size)
 
-def play():
+def Difficulty():
     while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        DIFF_MOUSE_POS = pygame.mouse.get_pos() #track mouse position on this screen
 
-        SCREEN.fill("black")
+        SCREEN.fill("black") #remove all rendered graphics by replacing it with a black screen
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+        DIFF_TEXT = get_font(45).render("Select Difficulty", True, "White")
+        DIFF_RECT = DIFF_TEXT.get_rect(center=(640, 100))
+        SCREEN.blit(DIFF_TEXT, DIFF_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(640, 460), 
+        DIFF_EASY = Button(image=None, pos=(640, 260), 
+                            text_input="EASY", font=get_font(75), base_color="White", hovering_color="Green")
+        DIFF_EASY.changeColor(DIFF_MOUSE_POS)
+        DIFF_EASY.update(SCREEN)
+
+        DIFF_HARD = Button(image=None, pos=(640, 360), 
+                            text_input="HARD", font=get_font(75), base_color="White", hovering_color="Green")
+        DIFF_HARD.changeColor(DIFF_MOUSE_POS)
+        DIFF_HARD.update(SCREEN)
+
+        DIFF_BACK = Button(image=None, pos=(640, 560), 
                             text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
+        DIFF_BACK.changeColor(DIFF_MOUSE_POS)
+        DIFF_BACK.update(SCREEN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                if DIFF_BACK.checkForInput(DIFF_MOUSE_POS):
                     main_menu()
+                if DIFF_EASY.checkForInput(DIFF_MOUSE_POS):
+                    easy_mode()
+                if DIFF_HARD.checkForInput(DIFF_MOUSE_POS):
+                    hard_mode()
 
         pygame.display.update()
 
@@ -100,49 +114,63 @@ def credits():
 
 def main_menu():
     while True:
-        SCREEN.fill("Black")
-        SCREEN.blit(BG, (0, 0))
+        SCREEN.fill("Black") #fill the screen black so that any extra graphics are removed and the screen is fresh
+        SCREEN.blit(BG, (0, 0)) #add the cave background image at position 0,0
 
 
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
+        MENU_MOUSE_POS = pygame.mouse.get_pos() #get the position of the mouse
 
-        MENU_TEXT = get_font(75).render("Miner Odyssey", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        TITLE_TEXT = get_font(75).render("Miner Odyssey", True, "#b68f40") #make variable for the main title
+        TITLE_RECT = TITLE_TEXT.get_rect(center=(640, 100)) #make variable for main title position
 
-        HYPERLINK_TEXT = get_font(15).render("Click on Joe to see what PickHacks is up to!", True, "Green")
-        HYPERLINK_RECT = HYPERLINK_TEXT.get_rect(center=(950,700))
+        HYPERLINK_TEXT = get_font(15).render("Click on Joe to see what PickHacks is up to!", True, "Green") #make varable for the text under joe
+        HYPERLINK_RECT = HYPERLINK_TEXT.get_rect(center=(950,700)) #make variable for hyperlink position
 
+        # make variables for all main menu buttons
         PLAY_BUTTON = Button(image=pygame.image.load("Assets/button.png"), pos=(640, 250), 
-                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
         CREDITS_BUTTON = Button(image=pygame.image.load("Assets/button.png"), pos=(640, 400), 
-                            text_input="CREDITS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="CREDITS", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
         QUIT_BUTTON = Button(image=pygame.image.load("Assets/button.png"), pos=(640, 550), 
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
         HYPERLINK_BUTTON = Button(image = pygame.image.load("Assets/JoeMinerHead.png"),pos=(1150,600),
-                            text_input = "", font = get_font(0), base_color = "#d7fcd4", hovering_color="White")
+                            text_input = "", font = get_font(0), base_color = "#d7fcd4", hovering_color="Green")
 
-        SCREEN.blit(MENU_TEXT, MENU_RECT)
+        # render main title and hyperlink text
+        SCREEN.blit(TITLE_TEXT, TITLE_RECT)
         SCREEN.blit(HYPERLINK_TEXT, HYPERLINK_RECT)
 
+        # loop to check if mouse position is on any button, if yes, change color from base to hover value
         for button in [PLAY_BUTTON, CREDITS_BUTTON, QUIT_BUTTON, HYPERLINK_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
         
+        #start to check for events
         for event in pygame.event.get():
+
+            #if the topright x is clicked, exit game
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            #else, check to see if a button was clicked
             if event.type == pygame.MOUSEBUTTONDOWN:
+                #if play button is pushed, go to difficulty function
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    play()
+                    Difficulty()
+                #if credits button pushed, go to credit function
                 if CREDITS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     credits()
+                #if quit button pushed, end program
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+                #if joe miner pushed, run hyperlink function
                 if HYPERLINK_BUTTON.checkForInput(MENU_MOUSE_POS):
                   hyperlink()
 
+        #display all changes made this cycle
         pygame.display.update()
 
+#when program is started, run main_menu function
 main_menu()
