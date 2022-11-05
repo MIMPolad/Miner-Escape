ee36164711e3b95fa1809079c05f9d387369fbb9
jrh48k <117498692+jrh48k@users.noreply.github.com>
2022-11-04 23:32:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
-        
+    print("Type 1 for Easy mode: ")
+    print("Type 2 for Hard mode: ")
+    print("Type 3 to Quit: ")
+    usrin = "true"
+    while(usrin != 1 and usrin != 2 and usrin != 3):
+        print("give a valid input (1, 2, or 3)
+        usrin = input(int())
+
 
 
 
 
 if __name__ == "__main__"":
-        main()
+     main()
