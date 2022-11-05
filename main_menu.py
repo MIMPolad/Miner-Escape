7173cb1adbf4ca8b97c829ba0b779336ac5b1cf7
MIMPolad <116120965+MIMPolad@users.noreply.github.com>
2022-11-05 01:21:44 -0500

    main menu needs joe head and font change

--- main_menu.py
+++ main_menu.py
@@ -4,12 +4,12 @@ from button import Button
 pygame.init()
 
 SCREEN = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Menu")
+pygame.display.set_caption("Miner Odyssey")
 
-BG = pygame.image.load("rock.png")
+BG = pygame.image.load("Assets/caveMenu.png")
 
 def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("font.ttf", size)
+    return pygame.font.Font("Assets/font.ttf", size)
 
 def play():
     while True:
@@ -69,14 +69,14 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_TEXT = get_font(100).render("Miner Odyssey", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("rock.png"), pos=(640, 250), 
+        PLAY_BUTTON = Button(image=pygame.image.load("Assets/bruh.png"), pos=(640, 250), 
                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("rock.png"), pos=(640, 400), 
+        OPTIONS_BUTTON = Button(image=pygame.image.load("Assets/bruh.png"), pos=(640, 400), 
                             text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("rock.png"), pos=(640, 550), 
+        QUIT_BUTTON = Button(image=pygame.image.load("Assets/bruh.png"), pos=(640, 550), 
                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
